ee452aadc932ecc4a21c4847df3c90dcb1ac0929
p3t1tpa1n <paul.pfl03@gmail.com>
2024-11-19 13:37:54 +0100

    Message du commit

--- c-wire.sh
+++ c-wire.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-echo "Bienvenue dans le script C-Wire"
+# Script de filtrage pour le projet C-Wire
+# Usage : ./c-wire.sh chemin_du_fichier.csv type_station type_consommateur
 
-if [ -z "$1" ]; then 
-	echo "erreur: aucun fichier fourni"
-	echo "utilisation: ./c-wire.sh chemin_du_fichier_fournie.csv"
-	exit 1
+
+# Vérification des paramètres
+if [ "$#" -lt 3 ]; then
+  echo "Erreur : Nombre de paramètres incorrect."
+  echo "Usage : ./c-wire.sh chemin_du_fichier.csv type_station type_consommateur"
+  exit 1
 fi
 
+# Paramètres
 fichier_csv="$1"
+type_station="$2"
+type_consommateur="$3"
 
-echo "fichier a traiter : $fichier_csv"
+# Vérifier si le fichier existe
+if [ ! -f "$fichier_csv" ]; then
+  echo "Erreur : Le fichier $fichier_csv n'existe pas."
+  exit 1
+fi
 
+# Créer un dossier temporaire
 mkdir -p tmp
 
-echo "traitement terminé. Les résultat sont dans le dossier tmp"
+# Filtrer les lignes selon les paramètres (exemple avec grep)
+if [ "$type_station" == "hvb" ]; then
+  grep "HV-B" "$fichier_csv" > tmp/filtre_hvb.csv
+  echo "Lignes HV-B filtrées dans tmp/filtre_hvb.csv"
+fi
 
-chmod +x c-wire.sh
-
+# Terminer le script
+echo "Traitement terminé."
